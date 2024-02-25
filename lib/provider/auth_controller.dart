f837620c112d4e0f8b1e13a83949b0fd84ab3f4a
AkiraKumoshita <kumo@MacBook-Pro-AK.local>
2024-02-25 15:17:33 +0900

    不要なimportを削除

--- lib/provider/auth_controller.dart
+++ lib/provider/auth_controller.dart
@@ -2,7 +2,6 @@ import 'package:demoapp/custom_exception.dart';
 import 'package:demoapp/provider/firebase_auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'auth_controller.g.dart';
